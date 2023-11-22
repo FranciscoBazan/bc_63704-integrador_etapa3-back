43650c8a657d017e2f2544de61cccbf3396c192b
Francisco Uriel  Bazán <franbazan014@gmail.com>
2023-11-22 18:25:37 -0300

    Agrego validaciones a routerClientes

--- routers/clientes.router.js
+++ routers/clientes.router.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import controller from '../controllers/clientes.controller.js'
+import validator from '../validators/clientes.validator.js'
 
 const routerClientes = express.Router()
 
-routerClientes.post('/', controller.guardarClientes)
+routerClientes.post('/', validator.clientesValidator, controller.guardarClientes)
 
 export default routerClientes
---
+++ validators/clientes.validator.js
@@ -0,0 +1,29 @@
+import { check } from "express-validator";
+import clientesMiddleware from "../middlewares/clientes.middleware.js";
+
+
+const clientesValidator = [
+    check('motivo')
+        .not().isEmpty().withMessage('Motivo es necesario'),
+    check('nombre')
+        .not().isEmpty().withMessage('El nombre es necesario')
+        .trim(),
+    check('mail')
+        .not().isEmpty().withMessage('El email es necesario')
+        .isEmail().withMessage('La información enviada no es un email válido')
+        .trim(),
+    check('telefono')
+        .not().isEmpty().withMessage('El teléfono es necesario')
+        .isNumeric().withMessage('El teléfono debe ser un número')
+        .isLength( { max: 10 } ).withMessage('El teléfono no puede tener más de 10 números')
+        .trim(),
+    check('comentarios')
+        .optional()
+        .isLength( { max: 700 } ).withMessage('Los comentarios no pueden superar los 700 caracteres')
+        .trim(),
+    clientesMiddleware
+]
+
+export default {
+    clientesValidator
+}
